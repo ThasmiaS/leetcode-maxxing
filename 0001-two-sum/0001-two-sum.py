96b4774451c547e447550b16647b6b499620fc09
Thasmia Showmir <123399342+ThasmiaS@users.noreply.github.com>
2026-02-16 15:39:32 -0500

    Time: 0 ms (100%), Space: 20.5 MB (43.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indices = []
+        #hashmap --> O(n)
+        map = {}
+        
+        for i,n in enumerate(nums):
+            complement = target - n #target = n + complement
+            if complement in map:
+                return [map[complement], i]
+            map[n] = i
+        
+        #BRUTE FORCE - O(n^2)
+        '''indices = []
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                   return [i,j]
+                   return [i,j]'''
